c75805abe51643224571a1fef4228768ff3f5d5d
athiwat0712253 <athiwat07122543@gmail.com>
2025-07-16 21:21:16 +0700

    Fix Cart.jsx

--- src/components/user/Cart.jsx
+++ src/components/user/Cart.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router";
 import numeral from "numeral";
-const API_BASE_URL = import.meta.env.VITE_BACKEND
+const VITE_BACKEND = import.meta.env.VITE_BACKEND
 
 const Cart = () => {
   const cart = useStore((state) => state.cart);
